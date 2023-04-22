a47dbe1d01385e93ef5b09795a77a8bae6228943
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-04-22 06:04:44 +0530

    back to top updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
-import { useCallback, useRef, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import usePosts from "../hooks/usePosts"
 import Post from "./Post"
 import Hero from "./Hero"
 import uparrow from '../assets/up-arrow.png'
+import { BiArrowFromBottom } from 'react-icons/bi'
+import { classNames } from "../hooks/classNames"
 
 const Home = () => {
     const [pageNum, setPageNum] = useState(1)
@@ -38,15 +40,54 @@ const Home = () => {
         return <Post key={post.id} post={post} />
     })
 
+    //back to top
+
+    const [isVisible, setIsVisible] = useState(false)
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setIsVisible(true)
+        } else {
+            setIsVisible(false)
+        }
+    }
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility)
+
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility)
+        }
+    }, [])
+
     return (
         <>
             <div className="bg-gray-100">
                 <Hero />
                 {content}
                 {isLoading && <p className="flex justify-center items-center font-mono font-bold">Loading more posts...</p>}
-                <a href="#" className="fixed bottom-4 end-3 scroll-smooth">
+                {/* <a href="#" className="fixed bottom-4 end-3 scroll-smooth">
                     <img src={uparrow} className="w-10 h-10" />
-                </a>
+                </a> */}
+                <div className="fixed bottom-2 right-2">
+                    <button
+                        type="button"
+                        onClick={scrollToTop}
+                        className={classNames(
+                            isVisible ? 'opacity-100' : 'opacity-0',
+                            'bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 inline-flex items-center rounded-full p-3 text-white shadow-sm transition-opacity focus:outline-none focus:ring-2 focus:ring-offset-2',
+                        )}
+                    >
+                        <BiArrowFromBottom className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                </div>
             </div>
         </>
     )
